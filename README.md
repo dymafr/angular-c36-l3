cd470def2ec70a704fd6475b0545c47f3242c463
dymafr <32801166+dymafr@users.noreply.github.com>
2022-12-08 19:36:54 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-c36-l3
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-c36-l3)
---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { AppComponent } from './app.component';
+import { InputBankCodeComponent } from './input-bank-code/input-bank-code.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    FormsModule,
+    FlexLayoutModule
+  ],
+  declarations: [AppComponent, InputBankCodeComponent],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
